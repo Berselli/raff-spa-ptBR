8d2abc8a5c15040f3d9202df59b01b884bf4ad9c
Juliano César Berselli <bersellib@gmail.com>
2019-09-25 10:58:09 -0300

    Criadas rotinas no gulp para mover arquivos javascript do Jquery, Popper e Bootstrap para a pasta src/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const watch = require('gulp-watch');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const watch = require('gulp-watch')
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/*.scss').pipe(sass({
         outputStyle: 'compressed'
     }).on('error', sass.logError)).pipe(gulp.dest('src/css'))
+})
+
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js']).pipe(gulp.dest('src/js'))
 })
 
 gulp.task('watch', function () {
